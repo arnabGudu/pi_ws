c52e8a871e47fd02c08892eb8262f4c1eaba719f
iarc <arnabgudu.gs@gmail.com>
2019-10-09 11:26:39 +0530

    pi

--- src/pibot/src/motor.py
+++ src/pibot/src/motor.py
@@ -8,32 +8,34 @@ IN = [16, 18, 29, 31]
 io.setmode(io.BOARD)
 io.setup(EN, io.OUT, initial=io.LOW)
 io.setup(IN, io.OUT, initial=io.LOW)
-io.output(IN, (io.HIGH, io.LOW, io.HIGH, io.LOW))
+io.output(IN, (io.LOW, io.HIGH, io.LOW, io.HIGH))
 
 en1 = io.PWM(EN[0], 1000)
-en1.start(0)	
+en1.start(50)	
 en2 = io.PWM(EN[1], 1000)
-en2.start(0)
+en2.start(50)
 
 def callback(data):
-	pwm.ChangeDutyCycle(50 + data)
-	pwm.ChangeDutyCycle(50 - data)
+	en1.ChangeDutyCycle(50 + data.data)
+	en2.ChangeDutyCycle(50 - data.data)
+
+def stop():
+	en1.stop()
+	en2.stop()
+	io.output(EN, io.LOW)
+	io.output(IN, io.LOW)
+	io.cleanup()
 
 def listener():
 	rospy.init_node('motor', anonymous = True)
 	rospy.Subscriber('speed', Int16, callback)
 	rospy.spin()
+	stop()
 
 if __name__ == '__main__':
-	try:
-		listener()
+	listener()
 
-	except KeyboardInterrupt:
-		    en1.stop()
-		    en2.stop()
-		    io.output(EN, io.LOW)
-		    io.output(IN, io.LOW)
-		    io.cleanup()	
+	
 			
 
 
